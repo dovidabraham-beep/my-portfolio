5219392fd898fa3b09809d0f68edc24d0516d61e
David Abraham <david.abraham@auxiom.com>
2026-04-19 21:24:40 -0400

    trigger fresh deploy

--- api/chat.js
+++ api/chat.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
     body: JSON.stringify(req.body),
   });
 
+  
   const data = await response.json();
   console.log("Anthropic response:", JSON.stringify(data));
   res.status(200).json(data);
